7d898beb960d067d8aaac5d488ab4a53165b07f1
pierluigi <pierluigibaiano@gmail.com>
2022-01-03 13:44:07 +0100

    split the about component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./about.scss";
 
 function About() {
-  return <div className="about">about</div>;
+  return (
+    <div className="about">
+      <div className="a_left">left</div>
+      <div className="a_right">right</div>
+    </div>
+  );
 }
 
 export default About;
