d0f6680b7686e976aa750b8577ff2b932f922c1d
pierluigi <pierluigibaiano@gmail.com>
2022-01-03 18:37:13 +0100

    create a contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.scss";
 import { FaLinkedinIn } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsGithub } from "react-icons/bs";
 
 function Contact() {
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="contact">
       <div className="c_bg"></div>
       <div className="c_wrapper">
         <div className="c_left">
-          <h2 className="c_title"> Let's discuss your project</h2>
+          <h2 className="c_title"> Contact me!</h2>
           <div className="c_info">
             <div className="c_info_item">
               <a
@@ -41,7 +47,19 @@ function Contact() {
             </div>
           </div>
         </div>
-        <div className="c_right"></div>
+        <div className="c_right">
+          <p className="c-description">
+            What's your story? Get in touch. Always available for for new
+            challenges if the right project comes along me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea placeholder="Message" name="name" rows="5"></textarea>
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
