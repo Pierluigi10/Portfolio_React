7f103a2e472925d8e6b5129c767db406ebd332b2
pierluigi <pierluigibaiano@gmail.com>
2022-01-03 14:11:57 +0100

    add a fake image in about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./about.scss";
+import codeGlasses from "../../images/codeglasses.png";
 
 function About() {
   return (
     <div className="about">
-      <div className="a_left">left</div>
+      <div className="a_left">
+        <div className="a_card bg"></div>
+        <div className="a_card">
+          <img src={codeGlasses} alt="" className="a_img" />
+        </div>
+      </div>
       <div className="a_right">right</div>
     </div>
   );
