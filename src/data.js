e966ed2865005528233215d59d3bfe783befbcdc
pierluigi <pierluigibaiano@gmail.com>
2022-03-08 10:27:28 +0100

    add new project (common destination) + change package.json

--- src/data.js
+++ src/data.js
@@ -4,40 +4,47 @@ import weather from "./images/weather.png";
 import matterjs from "./images/matterjs.png";
 import mern from "./images/mern.png";
 import secondPortfolio from "./images/secondportfolio.png";
+import commonDestination from "./images/commondestination.png";
 
 export const projectsData = [
   {
-    id: 1,
+    id: 7,
+    title: "Final Project: Common Destination",
+    img: commonDestination,
+    link: "https://commondestination.netlify.app/",
+  },
+  {
+    id: 6,
     title: "Portfolio after six months of study",
     img: secondPortfolio,
     link: "https://pierluigi.netlify.app/",
   },
   {
-    id: 2,
+    id: 5,
     title: "Project with matterJs",
     img: matterjs,
     link: "https://github.com/Pierluigi10/matterjsproject",
   },
   {
-    id: 3,
+    id: 4,
     title: "WeatherApp",
     img: weather,
     link: "https://whatistheweatherlike.netlify.app/",
   },
   {
-    id: 4,
+    id: 3,
     title: "MERN Showcase App",
     img: mern,
     link: "https://mernshowcase-pb-frontend.vercel.app/",
   },
   {
-    id: 5,
+    id: 2,
     title: "Funny game in JavaScript",
     img: game,
     link: "https://littlegamejs.netlify.app/",
   },
   {
-    id: 6,
+    id: 1,
     title: "My first portfolio after 1 months of study",
     img: firstPortfolio,
     link: "https://myfirstportfoliodci.netlify.app/",
